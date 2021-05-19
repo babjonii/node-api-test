d462561b76181be95c0c1c9bca3767a97d2cd7b5
babjonii <johthedon@gmail.com>
2021-05-19 11:44:39 +0300

    feat: handling get, post and delete functions of express

---
+++ dbService.js
@@ -0,0 +1,83 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: 'root',
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log('db ' + connection.state);
+    }
+});
+
+class DbService {
+    static getDbServiceInstance() {
+        return instance ? instance : new DbService();
+    }
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM names";
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            return response;
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async insertNewName(name) {
+        try {
+            const dateAdded = new Date();
+            const insertId = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO names(name, date_added) VALUES (?,?);";
+
+                connection.query(query, [name, dateAdded], (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.insertId);
+                })
+            });
+            return {
+                id: insertId,
+                name: name,
+                dateAdded: dateAdded
+            };
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async deleteRowById(id) {
+        try {
+            id = parseInt(id, 10);
+            const response = await new Promise((resolve, reject) => {
+                const query = "DELETE FROM names WHERE id = ?";
+
+                connection.query(query, [id], (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+
+            return response === 1 ? true : false;
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
+
+module.exports = DbService;
